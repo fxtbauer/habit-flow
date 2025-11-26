c30ad182e0fb0308b16b473a43944c417c7c8689
fxtbauer <ruanbauer16@gmail.com>
2025-11-26 00:24:26 -0300

    feat: initialize project structure with Maven, Javalin and hello world endpoint

--- src/main/java/br/gov/sp/fatec/pg/oo/Main.java
+++ src/main/java/br/gov/sp/fatec/pg/oo/Main.java
@@ -1,8 +1,21 @@
 package br.gov.sp.fatec.pg.oo;
 
+import io.javalin.Javalin;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("Teste inicial");
+
+        Javalin app = Javalin.create( config -> {
+        config.http.defaultContentType = "text/plain; charset = UTF-8";
+        config.pvt.javaLangErrorHandler((res, error) ->  {
+            res.setCharacterEncoding("UTF-8");
+        });
+        }).start(7070);
+        app.get("/", ctx ->  { 
+            // Teste
+            ctx.result("Hello World!");
+        });
     }
 }
