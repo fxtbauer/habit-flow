aa39b2b701ec77855b8160ce1d285b31b9673b6c
fxtbauer <ruanbauer16@gmail.com>
2025-11-25 23:37:12 -0300

    Initial commit

---
+++ src/main/java/br/gov/sp/fatec/pg/oo/Main.java
@@ -0,0 +1,8 @@
+package br.gov.sp.fatec.pg.oo;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println("Teste inicial");
+    }
+}
