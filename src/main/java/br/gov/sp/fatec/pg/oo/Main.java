a898a0a55a27f71930c5e93c8a9e77aaf600a84e
fxtbauer <ruanbauer16@gmail.com>
2025-12-02 03:01:10 -0300

    chore: adjust static file routes and clean routes

--- src/main/java/br/gov/sp/fatec/pg/oo/Main.java
+++ src/main/java/br/gov/sp/fatec/pg/oo/Main.java
@@ -6,27 +6,22 @@ import br.gov.sp.fatec.pg.oo.database.DatabaseInitializer;
 import io.javalin.Javalin;
 
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-        System.out.println("Teste inicial");
 
         DatabaseInitializer.initialize();
-        Javalin app = Javalin.create( config -> {
-        config.http.defaultContentType = "text/plain; charset = UTF-8";
-        config.pvt.javaLangErrorHandler((res, error) ->  {
-            res.setCharacterEncoding("UTF-8");
-        });
+
+        Javalin app = Javalin.create(config -> {
+            config.staticFiles.add("/static");
         }).start(7070);
+
         new UserController(app);
         new HabitController(app);
-        app.get("/status", ctx ->  { 
-            // Teste
-            ctx.result("API online");
-        });
 
+        app.get("/status", ctx -> ctx.result("API online"));
         app.get("/", ctx -> ctx.redirect("/login"));
+
         app.get("/login", ctx -> ctx.redirect("/login.html"));
         app.get("/dashboard", ctx -> ctx.redirect("/dashboard.html"));
-
     }
 }
